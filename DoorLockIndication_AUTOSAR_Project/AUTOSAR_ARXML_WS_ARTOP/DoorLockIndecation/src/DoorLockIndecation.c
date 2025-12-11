0b41ddcb4e1569a13370eec1228f7dc978f91028
Sohaibdar61 <sohaibdar61@gmail.com>
2025-12-11 20:00:18 +0200

    DoorLockIndicationProject

---
+++ DoorLockIndication_AUTOSAR_Project/AUTOSAR_ARXML_WS_ARTOP/DoorLockIndecation/src/DoorLockIndecation.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : DoorLockIndecation.c
+ Author      : SOHAIP-DAR
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!HELLO FOR EVEYONE FROM AUTOSAR CONCEPT!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
---
+++ DoorLockIndication_AUTOSAR_Project/Autosar_DoorLockIndecation/AUTOSAR_Implementation/AutosarIncludeFiles/Compiler.h
@@ -0,0 +1,13 @@
+/*
+ * Compiler.h
+ *
+ *  Created on: Feb 23, 2020
+ *      Author: SOHAIP-DAR
+ */
+
+#ifndef COMPILER_H_
+#define COMPILER_H_
+
+
+
+#endif /* COMPILER_H_ */
